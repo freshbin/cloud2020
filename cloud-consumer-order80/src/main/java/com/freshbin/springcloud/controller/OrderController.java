89cef978c235004675bf18029839f81785895f4f
freshbin <343625573@qq.com>
2020-04-07 23:00:17 +0800

    ribbon，OpenFeign，Hystrix

--- cloud-consumer-order80/src/main/java/com/freshbin/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/freshbin/springcloud/controller/OrderController.java
@@ -2,8 +2,11 @@ package com.freshbin.springcloud.controller;
 
 import com.freshbin.springcloud.entities.CommonResult;
 import com.freshbin.springcloud.entities.Payment;
+import com.freshbin.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,10 +32,10 @@ public class OrderController
     @Resource
     private RestTemplate restTemplate;
 
-//    @Resource
-//    private LoadBalancer loadBalancer;
-//    @Resource
-//    private DiscoveryClient discoveryClient;
+    @Resource
+    private LoadBalancer loadBalancer;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment)
@@ -46,7 +49,7 @@ public class OrderController
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
 
-    /*@GetMapping("/consumer/payment/getForEntity/{id}")
+    @GetMapping("/consumer/payment/getForEntity/{id}")
     public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)
     {
         ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
@@ -56,9 +59,9 @@ public class OrderController
         }else{
             return new CommonResult<>(444,"操作失败");
         }
-    }*/
+    }
 
-    /*@GetMapping(value = "/consumer/payment/lb")
+    @GetMapping(value = "/consumer/payment/lb")
     public String getPaymentLB()
     {
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
@@ -70,10 +73,10 @@ public class OrderController
 
         ServiceInstance serviceInstance = loadBalancer.instances(instances);
         URI uri = serviceInstance.getUri();
-
+        log.info("访问的服务器uri：" + uri.toString());
         return restTemplate.getForObject(uri+"/payment/lb",String.class);
 
-    }*/
+    }
 
     // ====================> zipkin+sleuth
    /* @GetMapping("/consumer/payment/zipkin")
--- cloud-provider-payment8001/src/main/java/com/freshbin/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/freshbin/springcloud/controller/PaymentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author freshbin
@@ -46,5 +47,19 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "没有对应记录，查询id：" + id, null);
         }
+    }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB()
+    {
+        return serverPort;
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout()
+    {
+        // 业务逻辑处理正确，但是需要耗费3秒钟
+        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+        return serverPort;
     }
 }
