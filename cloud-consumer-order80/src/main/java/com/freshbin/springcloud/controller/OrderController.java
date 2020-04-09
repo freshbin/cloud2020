7e03b578734a83bc7e6281d12c2cc85e84a4b73c
freshbin <343625573@qq.com>
2020-04-09 22:35:34 +0800

    nacos,sentinel

--- cloud-consumer-order80/src/main/java/com/freshbin/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/freshbin/springcloud/controller/OrderController.java
@@ -79,10 +79,10 @@ public class OrderController
     }
 
     // ====================> zipkin+sleuth
-   /* @GetMapping("/consumer/payment/zipkin")
+    @GetMapping("/consumer/payment/zipkin")
     public String paymentZipkin()
     {
         String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
         return result;
-    }*/
+    }
 }
--- cloud-provider-payment8001/src/main/java/com/freshbin/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/freshbin/springcloud/controller/PaymentController.java
@@ -62,4 +62,10 @@ public class PaymentController {
         try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
         return serverPort;
     }
+
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin()
+    {
+        return "hi ,i'am paymentzipkin server fall back，welcome to atguigu，O(∩_∩)O哈哈~";
+    }
 }
